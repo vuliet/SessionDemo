8eaeadd954d96afd1089a970a7e3eefe793f17b1
ThaiVu <thai35.tv@gmail.com>
2023-10-12 17:22:46 +0700

    Add project files.

---
+++ SessionDemo/Controllers/ProductController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace SessionDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetSession")]
+        public IActionResult Get()
+        {
+            CountAccess(HttpContext);
+
+            var sessionData = CountAccessInfo(HttpContext);
+
+            return Ok(new { data = sessionData});
+        }
+
+        private void CountAccess(HttpContext context)
+        {
+            // Lấy ISession
+            var session = context.Session;
+            string key_access = "info_access";
+
+            // Lưu vào  Session thông tin truy cập
+            // Định nghĩa cấu trúc dữ liệu lưu trong Session
+            var accessInfoType = new
+            {
+                count = 0,
+                lasttime = DateTime.Now
+            };
+
+            // Đọc chuỗi lưu trong Sessin với key = info_access
+            string json = session.GetString(key_access);
+            dynamic lastAccessInfo;
+            if (json != null)
+            {
+                // Convert chuỗi Json - thành đối tượng có cấu trúc như accessInfoType
+                lastAccessInfo = JsonConvert.DeserializeObject(json, accessInfoType.GetType());
+            }
+            else
+            {
+                // json chưa từng lưu trong Session, accessInfo lấy bằng giá trị khởi  tạo
+                lastAccessInfo = accessInfoType;
+            }
+
+            // Cập nhật thông tin
+            var accessInfoSave = new
+            {
+                count = lastAccessInfo.count + 1,
+                lasttime = DateTime.Now
+            };
+
+            // Convert accessInfo thành chuỗi Json và lưu lại vào Session
+            string jsonSave = JsonConvert.SerializeObject(accessInfoSave);
+            session.SetString(key_access, jsonSave);
+            Console.WriteLine(jsonSave);
+        }
+
+        private static string CountAccessInfo(HttpContext context)
+        {
+            var session = context.Session;          // Lấy ISession
+            string key_access = "info_access";
+
+            // Lưu vào  Session thông tin truy cập
+            // Định nghĩa cấu trúc dữ liệu lưu trong Session
+            var accessInfoType = new
+            {
+                count = 0,
+                lasttime = DateTime.Now
+            };
+
+            // Đọc chuỗi lưu trong Sessin với key = info_access
+            string json = session.GetString(key_access);
+            dynamic lastAccessInfo;
+            if (json != null)
+            {
+                // Convert chuỗi Json - thành đối tượng
+                lastAccessInfo = JsonConvert.DeserializeObject(json, accessInfoType.GetType());
+            }
+            else
+            {
+                // json chưa từng lưu trong Session, accessInfo lấy bằng giá trị khởi  tạo
+                lastAccessInfo = accessInfoType;
+            }
+            if (lastAccessInfo.count == 0)
+            {
+                return "Chưa truy cập /Product lần  nào";
+            }
+
+            string thongtin = $"Số lần truy cập /Product: {lastAccessInfo.count}  - lần cuối: {lastAccessInfo.lasttime.ToLongTimeString()}";
+            return thongtin;
+        }
+    }
+}
---
+++ SessionDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDistributedMemoryCache();           // Đăng ký dịch vụ lưu cache trong bộ nhớ (Session sẽ sử dụng nó)
+builder.Services.AddSession(cfg => {                    // Đăng ký dịch vụ Session
+    cfg.Cookie.Name = "thaivu";                         // Đặt tên Session - tên này sử dụng ở Browser (Cookie)
+    cfg.IdleTimeout = new TimeSpan(0, 60, 0);           // Thời gian tồn tại của Session
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseSession();                               // Đăng ký Middleware Session vào Pipeline
+
+app.Run();
